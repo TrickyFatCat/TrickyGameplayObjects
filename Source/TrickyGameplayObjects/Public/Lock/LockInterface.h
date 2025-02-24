3328a23e02b8d69cf617cda6390cef720aefdf57
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-02-24 22:01:08 +0200

    Add documentation for ILockInterface functions

--- Source/TrickyGameplayObjects/Public/Lock/LockInterface.h
+++ Source/TrickyGameplayObjects/Public/Lock/LockInterface.h
@@ -11,6 +11,9 @@ class AActor;
 
 DECLARE_LOG_CATEGORY_EXTERN(LogLock, Log, All);
 
+/**
+ * Represents the various states of a lock.
+ */
 UENUM(BlueprintType)
 enum class ELockState: uint8
 {
@@ -45,33 +48,68 @@ class ULockInterface : public UInterface
 };
 
 /**
- * 
+ * Interface for managing lock functionalities such as locking, unlocking, enabling, and disabling.
+ * This interface is designed to standardize lock interactions and transitions.
  */
 class TRICKYGAMEPLAYOBJECTS_API ILockInterface
 {
 	GENERATED_BODY()
 
 public:
+	/**
+	 * Attempts to lock a lock using a component (which implements KeyRingInterface) from a given actor.
+	 *
+	 * @param OtherActor The actor that interacts with the lock and has KeyRingComponent.
+	 * @param bTransitImmediately Determines if the lock should transit to the locked state immediately or not.
+	 * @return Returns true if the lock was successfully locked on started a transition to locked state.
+	 */
 	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = Lock)
 	bool Lock(AActor* OtherActor, const bool bTransitImmediately);
 
 	virtual bool Lock_Implementation(AActor* OtherActor, const bool bTransitImmediately);
 
+	/**
+	 * Attempts to unlock a lock using a component (which implements KeyRingInterface) from a given actor.
+	 *
+	 * @param OtherActor The actor that interacts with the lock and has KeyRingComponent.
+	 * @param bTransitImmediately Determines if the lock should transit to the unlocked state immediately or not.
+	 * @return Returns true if the lock was successfully unlocked on started a transition to unlocked state.
+	 */
 	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = Lock)
 	bool Unlock(AActor* OtherActor, const bool bTransitImmediately);
 
 	virtual bool Unlock_Implementation(AActor* OtherActor, const bool bTransitImmediately);
 
+	/**
+	 * Attempts to disable a lock.
+	 *
+	 * @param bTransitImmediately Determines if the lock should transit to the disabled state immediately or not.
+	 * @return Returns true if the lock was successfully disabled on started a transition to disabled state.
+	 */
 	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = Lock)
 	bool Disable(const bool bTransitImmediately);
 
 	virtual bool Disable_Implementation(const bool bTransitImmediately);
 
+	/**
+	 * Attempts to enable a lock.
+	 *
+	 * @param bTransitImmediately Determines if the lock should transit to the enabled (usually the last before disabling) state immediately or not.
+	 * @return  True if the lock was successfully enabled or started a transition from the enabled to last state before disabling.
+	 */
 	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = Lock)
 	bool Enable(const bool bTransitImmediately);
 
 	virtual bool Enable_Implementation(const bool bTransitImmediately);
 
+	/**
+	 * Attempts to force a lock to enter specific state.
+	 * @warning The lock can't be forced to Transition state.
+	 *
+	 * @param NewState The desired state to force the lock into.
+	 * @param bTransitImmediately Determines if the lock should transit to NewState immediately or not.
+	 * @return True if the lock successfully changed the state or started a transition to NewState.
+	 */
 	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = Lock)
 	bool ForceState(const ELockState NewState, const bool bTransitImmediately);
 
