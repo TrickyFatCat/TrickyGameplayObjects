0070dbce46a8322d02b1e1d0e80cee35b3d54d73
Artyom Volkov <tricky.fat.cat@gmail.com>
2025-02-27 17:46:44 +0200

    Update LockKeyType documentation comment

--- Source/TrickyGameplayObjects/Public/LockKey/LockKeyType.h
+++ Source/TrickyGameplayObjects/Public/LockKey/LockKeyType.h
@@ -7,7 +7,7 @@
 #include "LockKeyType.generated.h"
 
 /**
- * 
+ * A key type which is used to lock and unlock locks.
  */
 UCLASS(Blueprintable, BlueprintType)
 class TRICKYGAMEPLAYOBJECTS_API ULockKeyType : public UObject
